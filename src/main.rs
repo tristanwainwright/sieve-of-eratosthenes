0c9997f5f20b511bf571dd435b91625b8f3c99c6
Tristan Wainwright <wainwright@disroot.org>
2024-02-21 22:24:43 -0500

    implement prime sieve algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+mod helpers;
+use helpers::isqrt;
+
+fn sieve_of_eratosthenes(n: u32) -> Vec<u32> {
+    let mut is_prime_v: Vec<bool> = vec![true; usize::try_from(n).unwrap()];
+
+    is_prime_v[0] = false;
+    is_prime_v[1] = false;
+
+    for i in 2..=isqrt(n) {
+        if is_prime_v[i as usize] {
+            let mut j: u32 = i.pow(2);
+            let mut coefficent: u32 = 1;
+            while j < n {
+                is_prime_v[j as usize] = false;
+                j = i.pow(2) + coefficent * i;
+                coefficent += 1;
+            }
+        }
+    }
+
+    let mut primes: Vec<u32> = Vec::new();
+
+    for (pos, is_prime) in is_prime_v.iter().enumerate() {
+        if *is_prime {
+            primes.push(pos as u32);
+        }
+    }
+
+    return primes;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: u32 = 30;
+    let primes: Vec<u32> = sieve_of_eratosthenes(n);
+    println!("{:?}", primes);
 }
