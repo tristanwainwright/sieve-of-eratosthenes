8dec0e793156f51fc9f4a76a330e3e509483ec3e
Tristan Wainwright <wainwright@disroot.org>
2024-02-22 23:51:11 -0500

    add handling for valid n values

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
 
+const N_HELP: &str = "Upper limit for generation, must be >= 2";
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, allow_negative_numbers = false)]
+    #[arg(short, value_parser = clap::value_parser!(u32).range(2..), help = N_HELP)]
     pub n: u32,
 }
