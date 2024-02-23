2aa41ec9affca43fa2826fd8f89dbde6bb27dcad
Tristan Wainwright <wainwright@disroot.org>
2024-02-22 22:49:11 -0500

    have clap pull from cargo manifest

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
 
-const VERSION: &str = "0.1.0";
-const ABOUT: &str = "Sieve algorithm to generate list of prime numbers from 2 to n.";
-
 #[derive(Parser, Debug)]
-#[command(version = VERSION, about = ABOUT, long_about = None)]
+#[command(version, about, long_about = None)]
 pub struct Args {
     #[arg(short, allow_negative_numbers = false)]
     pub n: u32,
